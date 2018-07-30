04b754243661e008bc6bcff9f62421716f1caf08
AmerChahda <amer.chahda@sigma.se>
2018-07-30 22:38:06 +0200

    Finale commit

--- Count_distinct_words_main.cpp
+++ Count_distinct_words_main.cpp
@@ -1,75 +1,70 @@
-/*
- * Count_distinct_words_main.cpp
- *
- *  Created on: 4 juli 2018
- *      Author: ACH11739
- */
-/*#include <iostream>
-#include <string>
-#include <vector>
+
+#include <iostream>
+#include <list>
 #include <iterator>
+#include <sstream>
 
-int main() {
-	std::vector<std::string>p_vector;
-	std::string word;
-	std::cout<<"Enter your sentence to check how many words: "<<std::endl;
-	std::istream_iterator<std::string>eos;              // end-of-stream iterator
-	std::istream_iterator<std::string>iit(std::cin);
-	//std::getline(std::cin,word);
-
-
-	while(iit!=eos){
-		p_vector.push_back(*iit);
-		++iit;
+void printing_list(std::list<std::string>l_w,int dis_words_counter)
+{
+	std::cout<<"The total number of the distinct words are: "<<dis_words_counter<<"\nThees words are: ";
+	for(auto i=std::begin(l_w);i!=std::end(l_w);++i)
+	{
+		std::cout<<*i<<"\t\n";
 	}
-	for(std::vector<std::string>::iterator i=p_vector.begin();i!=p_vector.end();++i)
-		std::cout<<*i<<"\n";
+}
+void chech_the_rebetition_words(std::list <std::string>&l_w, std::list<std::string>&finel_list,int &counter)
+{
+	std::string temp;
+	bool eq=false;
+	while(!l_w.empty())
+	{
+		temp=l_w.front();
+		l_w.pop_front();
+		if (finel_list.empty())
+		{
+			finel_list.push_back(temp);
+			counter++;
+		}
+		else
+		{
+			for(auto i=std::begin(finel_list);i!=std::end(finel_list);++i)
+			{
+
+				if (*i==temp)
+				{
+					eq=true;
+					break;
+				}
+			}
+			if(!eq)
+			{
+				finel_list.push_back(temp);
+				counter++;
+			}
+			eq=false;
+		}
+	}
+	printing_list(finel_list,counter);
+}
+int main()
+{
+	int counter=0;
+		std::string value;
+		std::string input_stream;
+		std::list<std::string>word;
+		std::list<std::string>dis_list;
+
+		std::cout << "Please, insert your sentence: "<<std::endl;
+
+		if (std::getline(std::cin,input_stream))
+		{
+			std::istringstream iss(input_stream);
+			std::copy(std::istream_iterator<std::string>(iss), std::istream_iterator<std::string>(), std::back_inserter(word));
+
+		}
+		chech_the_rebetition_words(word,dis_list,counter);
+
 	return 0;
 }
-*/
 
-// istream_iterator example
-#include <iostream>     // std::cin, std::cout
-#include <iterator>     // std::istream_iterator
-#include <vector>
-
-int main () {
-	int counter=0;
-	std::string value;
-	std::vector<std::string>word;
-  std::cout << "Please, insert your sentence: "<<std::endl;
-
-  std::istream_iterator<std::string> eos;              // end-of-stream iterator
-  std::istream_iterator<std::string> iit (std::cin);   // stdin iterator
-  std::cout<<"\n";
-  word.push_back(*iit);
-  ++iit;
-  ++counter;
-  while(iit!=eos)
-  {
-	  value=*iit;
-	 for (int i=0;i<word.size();++i)
-	 {
-		 if(value==word[i])
-		 {
-			 break;
-		 }
-		 else{
-			 if (i==(word.size()-1)){
-				 word.push_back(value);
-				 ++counter;
-			 }
-
-		 }
-	 }
-	 ++iit;
-  }
-std::cout<<"The total number of the distinct words are: "<<counter<<"\nThees words are: ";
-for(int i=0;i<word.size();i++){
-	std::cout<<word[i]<<"\t";
-}
-
-
-  return 0;
-}
 
